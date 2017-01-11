fd10bec2ebc74b99d726b325f76d96038370f1b9
shiju mathai <smathai@us.ibm.com>
2017-01-11 14:56:31 -0600

    testing object

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -146,16 +146,21 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 
     name = args[0]
     valAsbytes, err := stub.GetState(name)
+		var contractIds []string
+
+
 		var contract contract
+		json.Unmarshal(valAsbytes, &contract)
+		//asBytes, _ := json.Marshal(allContracts)
 
-    if err != nil {
-        jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
-        return nil, errors.New(jsonResp)
-    }
-	json.Unmarshal(valAsbytes,&contract)
-
+  //  if err != nil {
+    //    jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+    //    return nil, errors.New(jsonResp)
+  //  }
+//json.Unmarshal(valAsbytes,&contract)
 	//	asBytes, _ := json.Marshal(allContracts)
-	return valAsbytes, nil
+//	return valAsbytes, nil
+return valAsbytes,nil
 }
 
 // Query is our entry point for queries
